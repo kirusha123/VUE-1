ed232ac6f53c4add7aca5380ee90e9b924447985
kirusha123 <kirriess@mail.ru>
2021-07-26 02:22:38 +0300

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+    <div id="app">
+        <Hello msg="Hello World)"/>
+    </div>
+</template>
+
+<script>
+import Hello from './components/Hello';
+
+export default {
+    name: "App",
+    components: {
+        Hello
+    }
+}
+</script>
+
+<style>
+    #app{
+        display: flex;
+        justify-content: center;
+    }
+</style>
---
+++ src/components/Hello.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="hello">
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<script>
+    export default {
+        name : "Hello",
+        props : {
+            msg : String
+        }
+    };
+</script>
+
+<style scoped>
+    .hello{
+         flex:center;
+         width: 40%;
+         height: 40%;
+         text-align: center;
+    }
+</style>
